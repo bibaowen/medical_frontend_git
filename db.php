9c257922ad5504cdcab3ee319c9f29030e7941f6
bibaowen <bobby.owen@gmail.com>
2025-07-26 15:38:08 -0800

    Add files via upload

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$host = getenv('DB_HOST');
+$host = getenv('DB_HOST') ?: 'dpg-d22i0nidbo4c73f94l8g-a.oregon-postgres.render.com';
 $port = getenv('DB_PORT') ?: '5432';
 $dbname = getenv('DB_NAME');
 $user = getenv('DB_USER');
@@ -10,4 +10,3 @@ $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pa
 if (!$conn) {
     die("❌ Database connection failed: " . pg_last_error());
 }
-?>
